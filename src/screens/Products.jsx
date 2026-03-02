931cc2bacb8e40987bf3b556294f2aafc9b9b08c
Brayan Garzón <destroyergarzon@gmail.com>
2026-03-01 20:31:09 -0500

    Add gitignore

---
+++ src/screens/Products.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Products() {
+  return (
+    <div className="pt-32 px-6">
+      <h1 className="text-4xl font-bold text-gray-900">Bienvenido a DevMarket</h1>
+      <p className="mt-4 text-gray-600">Productos</p>
+    </div>
+  );
+}
